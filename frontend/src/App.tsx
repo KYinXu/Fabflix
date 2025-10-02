99f4b4e588e1dab5bb2244194855dc25e3a5b208
Kyle Yin Xu <kyleyinxu@gmail.com>
2025-10-01 19:50:11 -0700

    Add movie-list and movie pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MovieList from './features/movie-list/pages/movie_list';
+import Movie from './features/movies/pages/movie';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<MovieList />} />
+          <Route path="/movie/:id" element={<Movie />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/features/movie-list/pages/movie_list.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MovieList: React.FC = () => {
+  return (
+    <div className="movie-list">
+      <h1 className="text-3xl font-bold text-center mb-8">Movie List</h1>
+      <div className="container mx-auto px-4">
+        <p className="text-gray-600 text-center">
+          Filler text
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
