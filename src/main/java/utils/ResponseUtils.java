9b7b358494a2d7eb6caf46e700d5d369668e0c3a
Kyle Yin Xu <kyleyinxu@gmail.com>
2025-10-11 02:25:51 -0700

    Add potential utils file for future use

---
+++ src/main/java/utils/ResponseUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ResponseUtils {
+    public static void setCommonHeaders(HttpServletResponse response) {
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//        response.setHeader("Access-Control-Allow-Credentials", "true");
+//        response.setContentType("application/json; charset=UTF-8");
+    }
+}
