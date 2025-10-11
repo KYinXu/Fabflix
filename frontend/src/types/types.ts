ce6e7461dbb1575672b230b46a87a5d5c62e5b3e
Kyle Yin Xu <kyle@forgerewards.com>
2025-10-11 00:38:42 -0700

    Refactor types

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,26 @@
+export interface Movie {
+    id: string;
+    title: string;
+    year: number;
+    director: string;
+    stars?: Star[];
+    genres?: Genre[];
+    rating?: Rating;
+}
+
+export interface Star {
+    id: string;
+    name: string;
+    birth_year?: number;
+    movies?: Movie[];
+}
+
+export interface Genre {
+    id: number;
+    name: string;
+}
+
+export interface Rating {
+    ratings: number;
+    vote_count: number;
+}
