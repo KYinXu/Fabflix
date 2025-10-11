83f508acc8fb06d7c26660ef1b47bda2e0024358
Kyle Yin Xu <kyle@forgerewards.com>
2025-10-11 00:39:09 -0700

    Adjust database URL

--- src/main/java/MovieListServlet.java
+++ src/main/java/MovieListServlet.java
@@ -31,7 +31,7 @@ public class MovieListServlet extends HttpServlet{
         // MySQL Connection Information
         String loginUser = Parameters.username;
         String loginPassword = Parameters.password;
-        String loginUrl = "jdbc:mysql://localhost:3306/movie_db";
+        String loginUrl = "jdbc:" + Parameters.dbtype + ":///" + Parameters.dbname + "?autoReconnect=true&useSSL=false";
 
         // Response Mime Type
         response.setContentType("application/json");
