fd5ce2077ed319aab85319febdf476d385d0adf0
Kyle Yin Xu <kyleyinxu@gmail.com>
2025-10-11 03:13:55 -0700

    Add setup details in ParametersExample

--- src/main/java/ParametersExample.java
+++ src/main/java/ParametersExample.java
@@ -1,3 +1,5 @@
+
+// IMPORTANT: Rename this file and class to Parameters.java and fill in SQL login information
 public interface ParametersExample {
     String dbtype = "mysql";
     String dbname = "moviedb";
